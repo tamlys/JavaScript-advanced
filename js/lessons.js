66b5d2679b33a5e5266b03eee78bc26c4a66d1e7
Sachenkov Maxim <oqiwhite1996@gmail.com>
2021-05-07 11:02:48 +0300

    npm. JSON-server

--- js/lessons.js
+++ js/lessons.js
@@ -1,38 +1,38 @@
 'use strict';
 
 // filter
-// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
 
-// const shortNames = names.filter(name => {
-//     return name.length < 5;
-// });
+const shortNames = names.filter(name => {
+    return name.length < 5;
+});
 
-// console.log(shortNames);
+console.log(shortNames);
 
 // map
-// const answers = ['IvAn', 'AnnA', 'Hello'];
+const answers = ['IvAn', 'AnnA', 'Hello'];
 
-// const result = answers.map(item => item.toLowerCase());
+const result = answers.map(item => item.toLowerCase());
 
-// console.log(result);
+console.log(result);
 
 // every/some
-// const some = [4, 'qwerty', 'hi'];
-// const numbers = [1, 2, 3, 4];
+const some = [4, 'qwerty', 'hi'];
+const numbers = [1, 2, 3, 4];
 
-// console.log(some.some(item => typeof(item) === 'number'));
-// console.log(some.every(item => typeof(item) === 'number'));
-// console.log(numbers.every(item => typeof(item) === 'number'));
+console.log(some.some(item => typeof(item) === 'number'));
+console.log(some.every(item => typeof(item) === 'number'));
+console.log(numbers.every(item => typeof(item) === 'number'));
 
 // reduce
-// const arr = [4, 5, 1, 3, 2, 6];
-// const stringArr = ['apple', 'pear', 'plum'];
+const arr = [4, 5, 1, 3, 2, 6];
+const stringArr = ['apple', 'pear', 'plum'];
 
-// const res = arr.reduce((sum, current) => sum + current, 3);
-// const stringRes = stringArr.reduce((sum, current) => `${sum}, ${current}`);
+const res = arr.reduce((sum, current) => sum + current, 3);
+const stringRes = stringArr.reduce((sum, current) => `${sum}, ${current}`);
 
-// console.log(res);
-// console.log(stringRes);
+console.log(res);
+console.log(stringRes);
 
 const obj = {
     ivan: 'persone',
